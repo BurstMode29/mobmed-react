b7d19bc2075641a152417d0991a7f6cec7d26a5c
Rafeeq Weideman <r.w.weideman.rafeeq@gmail.com>
2022-10-21 10:11:40 +0100

    link

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: 'https://63512304bdd6760920e09ce5--mobmed-user.netlify.app',
+    url: 'https://mobmed-user.netlify.app',
     // This must be true.
     handleCodeInApp: true,
   };
