e6bd679da6d18c519f345f4a80dc41492e873353
Rafeeq Weideman <r.w.weideman.rafeeq@gmail.com>
2022-11-01 14:29:17 +0000

    update 1/11/22

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,10 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC4b-XmL7pR8I1DUbquhtRuhislv8Jg-CI",
-  authDomain: "mobmed-sign-up.firebaseapp.com",
-  projectId: "mobmed-sign-up",
-  storageBucket: "mobmed-sign-up.appspot.com",
-  messagingSenderId: "800242236705",
-  appId: "1:800242236705:web:34302c467c9261a14a1260"
-};
+//  Connection String
 
+ const {firebaseConfig} = require('../src/.env')
+ 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
